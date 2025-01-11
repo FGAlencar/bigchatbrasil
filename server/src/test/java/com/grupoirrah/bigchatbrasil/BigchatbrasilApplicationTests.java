6fa3eb30f5500d016dbddef5ccc6efb97e939d0e
Felipe Alencar <felipe.alencar@elotech.com.br>
2025-01-11 16:13:41 -0300

    Projeto inicial

---
+++ server/src/test/java/com/grupoirrah/bigchatbrasil/BigchatbrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoirrah.bigchatbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigchatbrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
